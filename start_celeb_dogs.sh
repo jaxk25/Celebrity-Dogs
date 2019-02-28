843d03afb71ad85402eaadd575166b9b73896b42
jaxk25 <32981635+jaxk25@users.noreply.github.com>
2019-02-28 10:54:06 +0000

    Add files via upload

---
+++ start_celeb_dogs.sh
@@ -0,0 +1,18 @@
+echo "Debian or Ubuntu based systems only!!"
+echo ""
+echo "Making sure dependencies are installed"
+echo ""
+sudo apt-get install python
+sudo apt-get install idle
+sudo apt-get install python-pip
+sudo pip install random
+echo ""
+echo "All done"
+echo ""
+echo "Now starting celebrity_dogs_game.py"
+echo ""
+python celebrity_dogs.py
+echo ""
+echo ""
+echo ""
+echo "Thank you for playing!"
