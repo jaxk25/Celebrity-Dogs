7e319dce7c41b83252fff4858b6bb4214eef97ea
jaxk25 <jaxk@jaxk.tk>
2019-03-28 10:25:59 +0000

    Delete start_celeb_dogs.bat

--- start_celeb_dogs.bat
+++
@@ -1,9 +0,0 @@
-echo "Windows systems only!"
-echo "Make sure you have python 2.7.X installed..."
-echo "Make sure you have python-pip installed..."
-echo "Make sure you have the random module installed..."
-echo ""
-echo "Now starting celebrity dogs game.py"
-python celebrity\ dogs\ game.py
-echo ""
-echo "Thanks for playing!"
